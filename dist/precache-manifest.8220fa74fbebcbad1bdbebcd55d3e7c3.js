984a3bddb07d346325dc9f52917fcebe955c4843
jeffrey <hins95@gmail.com>
2019-11-09 22:10:30 +0800

    add ./dist

---
+++ dist/precache-manifest.8220fa74fbebcbad1bdbebcd55d3e7c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9208aa6ec889fe04ce59",
+    "url": "/vendors.async.js"
+  },
+  {
+    "revision": "11ddd598045ef1f49bad",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "11ddd598045ef1f49bad",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "/pwacompat.min.js"
+  },
+  {
+    "revision": "a85242cc221e04f39ee4",
+    "url": "/p__index.async.js"
+  },
+  {
+    "revision": "91d66624d7d96849670b91c5f86d0f38",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c6e4b0ddcac660c9f243",
+    "url": "/layouts__index.async.js"
+  },
+  {
+    "revision": "db6ef46f28a4c621a1613b754afd2ccb",
+    "url": "/index.html"
+  }
+];
