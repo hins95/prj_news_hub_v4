e9dded78871907500e1e942bab3a911286728cef
jeffrey <hins95@gmail.com>
2019-11-10 16:20:06 +0800

    add meta

---
+++ dist/precache-manifest.e190fe3b01d34ca0f365258f9980131b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "686771a8050a58c7e0b0ccfc3fdf972b",
+    "url": "/prj_news_hub_v4/dist/index.html"
+  },
+  {
+    "revision": "c5f55a6fdce177fe203f",
+    "url": "/prj_news_hub_v4/dist/vendors.async.js"
+  },
+  {
+    "revision": "911504ec653962169024",
+    "url": "/prj_news_hub_v4/dist/umi.js"
+  },
+  {
+    "revision": "911504ec653962169024",
+    "url": "/prj_news_hub_v4/dist/umi.css"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "/prj_news_hub_v4/dist/pwacompat.min.js"
+  },
+  {
+    "revision": "6d97ac5aff10b98beb5c",
+    "url": "/prj_news_hub_v4/dist/p__index.async.js"
+  },
+  {
+    "revision": "576ef0bd04e4fd5c84b51a2276a5cd5c",
+    "url": "/prj_news_hub_v4/dist/manifest.json"
+  },
+  {
+    "revision": "79e23589eee35af27434",
+    "url": "/prj_news_hub_v4/dist/layouts__index.async.js"
+  },
+  {
+    "revision": "6fdbf6076a1d8f1fb58b90cb9ecd1bcb",
+    "url": "/prj_news_hub_v4/dist/assets/images/preview-screens/mobile.png"
+  },
+  {
+    "revision": "6601f792382ac1f1e61f36a62dcd2ccd",
+    "url": "/prj_news_hub_v4/dist/assets/images/apple-touch-icon-1024x1024.png"
+  },
+  {
+    "revision": "4197281241b4f63d30576c516089afaf",
+    "url": "/prj_news_hub_v4/dist/assets/images/.DS_Store"
+  }
+];
